1bc74628e545bb1396f52e8f649b6606108f998b
devtushar001 <tc341269@gmail.com>
2025-03-22 13:17:42 +0530

    All method to reverse a string

--- 06JSChallange/reverseStringMethod.js
+++ 06JSChallange/reverseStringMethod.js
@@ -21,3 +21,22 @@ console.log(sreverseString("Reverse a string with loop") + "--02")
 console.log(sreverseString("Testing") + "--02");
 
 // Using recursion
+
+function treverseString(str) {
+    if (str === "") return "";
+    return treverseString(str.substring(1)) + str.charAt(0);
+}
+
+console.log(treverseString("devtushar") + "--03");
+
+// Using reduce
+function freverseString(str) {
+    return str.split('').reduce((rev, char) => char + rev, "");
+}
+
+console.log(freverseString("asam") + "--04");
+
+//Using ES6 Spread Opearator
+
+const fireverseString = str => [...str].reverse().join('');
+console.log(fireverseString("Hello") +"--05")
